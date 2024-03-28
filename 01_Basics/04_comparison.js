7c7cb27690cd8dcac402b59d94231715d2227f5e
Akshat Jain <147633547+010Akshat@users.noreply.github.com>
2024-03-28 11:04:28 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2>1)// true
+// console.log(2>=1)//true
+// console.log(2<1)//false
+// console.log(2==1)//false
+// console.log(2!=1)//true
+
+// console.log("2">1)// true
+// console.log("02">1) // true
+// in such type of comparisons result cant be predicted , we should avoid comparing different data types
+
+console.log(null>0) // false
+console.log(null==0) // false
+console.log(null>=0) // true
+// we should avoid comparing different data types
+// The reason is that equality check == and comparisons > <  >= <= work differently
+// Comparison convert null to a number , treating it as 0.
+// That's why null >= 0 is true and null > 0 is false.
+// Here language has inconsistency because sometimes it converts null into Nan also.
+
+
+console.log(undefined==0)// false
+console.log(undefined>0)// false
+console.log(undefined<0) // false
+
+// Main point to be noted here is that comparison operators  > <  >= <= and ==works differently
+
+// === strict check
+console.log("2"===2)//false
