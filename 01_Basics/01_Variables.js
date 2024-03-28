51b516c27494eee9e74cf241146374e442215ece
Akshat Jain <147633547+010Akshat@users.noreply.github.com>
2024-03-28 09:54:35 +0000

    datatypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=2// not allowed
+accountEmail="akas@gmail.com"
+accountPassword="21323"
+accountCity="bengaluru"
+let accountState
+console.log(accountId)
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,21 @@
+"use Strict" // treat all JS Code in newer version
+
+// alert(3+3) // we are using nodejs .. in html file and in developer console it will pop up a box but their is different syntax in node
+console.log(3+3); console.log(3+3); // readabilty reduced
+// instead use separate lines and you donot require ; then
+
+let name="akshat"
+let age=21
+let isLoggedIn=false
+
+// DataTypes
+//number => 2 to power53
+// big int
+//string =>""  '' // prefer=>""
+// boolean => true / false
+// null =>standalone value
+// undefined => no value or datatype is defined // let account; is undefined
+// symbol => unique (will be used in react)
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object .. (it is said as drawback of language)
+ 
