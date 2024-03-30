3f985bcf3be820bcf02d1c8d04991a06913edf0e
Akshat Jain <147633547+010Akshat@users.noreply.github.com>
2024-03-30 07:05:56 +0000

    functions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,83 @@
+function sayMyName(){ // function keyword , sayMyName is name of function and rest syntax
+    console.log("A")
+    console.log("J")
+}
+sayMyName// it is only reference , when you run it no output will be generated
+sayMyName() // A [next line] J // adding () means execution that function will be executed 
+
+
+function addTwoNumbers(number1,number2){// here number1 and number2 are parameters 
+    console.log(number1+number2)
+}
+const result=addTwoNumbers(3,4) // 7 // 3,4 are arguments , when function is called 
+console.log(result) // undefined //as function did not return anything
+
+function addTwoNumbers1(number1,number2){
+    return (number1+number2)
+}
+const result1=addTwoNumbers1(3,4)
+console.log(result1)//7
+
+//---------------------------------------------------------------------------------------------------------
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("akshat")) //akshat just logged in
+//--------------------------------Interview ques, when you dont pass any argument --------------------
+console.log(loginUserMessage())//undefined  just logged in// so answer will be undefined logged in not any error
+
+// you can give default value to function
+function loginUserMessage1(username="DefaultName"){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage1()) //DefaultName just logged in
+
+function loginUserMessage2(username){
+    if(!username){
+        console.log("Please enter username ")
+        return 
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage2())// Please Enter UserName  [nextline] undefined
+
+//----------------------------------------------------------------------
+//  ... is used for both spread and rest operator 
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500))//[ 200, 400, 500 ]
+function calculateCartPrice1(val1,val2,...num1){
+    return num1
+}
+// -----------------Interview Question------------------------------------------------------------------------
+console.log(calculateCartPrice1(200,400,500,2000))//[ 500 ,20000 ] // as val1=200,val2=400 and rest funtion
+
+//------------------------------------------------------------------------------------------------
+const user = {
+    name:"Sam",
+    price:"999"
+}
+
+function handleObject(anyobject){
+    console.log(`UserName is ${anyobject.username} and price is ${anyobject.price}`)// UserName is undefined and price is 999 // name of property should be same, thats why when we fetch data using API we check for presence by using if else condition..
+}
+handleObject(user)
+handleObject({
+    username:"aj",
+    price:"100000"
+})//UserName is aj and price is 100000
+
+//-------------------------------------------------------------------
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+       console.log(getArray[1])
+}
+
+returnSecondValue(myNewArray)//400
+returnSecondValue([10,20,30])//20
---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,82 @@
+// {}  // These curly braces indicate scope of function or if else condition .. obejct curly braces are different.. vaise feel agyi hai 
+// var ke sath dikkat ye hi thi ki vo scope ke hisaab se nhi chalta tha , iss vajah se sab let ko use karte hai
+var c=300
+if(true){
+    let a =10
+    const b=20
+    c=30
+}
+// console.log(a)// error because a has scope inside if
+// console.log(b)// error because b has scope inside if
+console.log(c) //30 
+// Now it is a problem because c was initialised by some user as 300 and in some other file c=30.. this problem is created because var doesnt take care of scopes.. 
+// Thats why let is used instead of var in JS
+ // inside {} is is called block scope and outside it is global scope 
+ // variable declared in global scope can be used can be used inside block scope but not vice versa
+let v =300
+if(true){
+    let v=20
+    console.log(v)//20 // block scope
+}
+console.log(v)// 300 // global scope
+
+// ----------------------------------Very Important Point w.r.t Interview--------------------------
+/* Your global scope here in node ennvironment is different from global scope in developers console in google .
+It will discussed later using keyword . Till then rememeber the point */
+
+
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username) // hitesh// it can acccess of variables of outside level function one()
+    }
+    // console.log(website) // error // only inner level have access of outerlevel variables , website is inner level variable in function two
+    two()
+}
+one()
+// all these function calls have call stack which will be discussed later through diagrams 
+
+//------------------------Same Concept Applies for if else -----------------------------------------
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+        const website ="youtube"
+        console.log(username+website) // hitesh youtube
+    }
+ // console.log(website)// error 
+}
+
+// console.log(username) // error
+
+//+++++++++++++++++++++++++++++++++++++++ INTERESTING AND IMPORTANT++++++++++++++++++++++++++++++++++++++++++++++++
+// their are two ways of creating a function
+function addone(num){
+    return num+1
+}
+
+console.log(addone(5))// 6
+
+const addtwo=function(num){
+    return num+2
+}
+console.log(addtwo(7)) // 9
+
+//+++++++++++++++++++++++++++++ BUT ++++++++++++++++++++++++++++++++++
+
+console.log(addthree(5))// 8
+function addthree(num){
+    return num+3
+}
+
+
+console.log(addfour(7)) // error Cannot access 'addfour' before initialization
+const addfour=function(num){
+    return num+4
+}
+// the point to be noted here is when you call function before initialization it worked in first method but will give error in second method 
+// This is somehow related to hoisting which will be discussed later 
+
