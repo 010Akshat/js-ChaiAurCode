09c77567d46e4f2dd6c18a8618b9f21b066269b2
Akshat Jain <147633547+010Akshat@users.noreply.github.com>
2024-12-05 15:37:43 +0530

    Update 04_iife.js

--- 03_Basics/04_iife.js
+++ 03_Basics/04_iife.js
@@ -28,9 +28,14 @@ For example, it can be  used in database connectivity , as youy open the app you
 
 
 ((name)=>{ // parameter
-    // simple iife not simple iffe
+    // simple iife (unanamed iife) not simple iffe
    console.log(`DB connected two,${name}`)
 })("aj") // arrow functi0n can also be called   // arguments 
+
+// ------------------INTERVIEW SPECIFIC----------------
+//1. Why iife?  above two both are reasons , not only execute immediately . only this ans is wrong
+//2. Named iife and simple iife?
+//3. Question is asked write 2 iife? People do mistake by not writing ; which gives error. So rememver to write semicolon.
 
 
 
