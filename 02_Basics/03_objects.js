ea11e42639357f9a02fd191e327d7fd18570845c
Akshat Jain <147633547+010Akshat@users.noreply.github.com>
2024-12-05 13:12:05 +0530

    Update 03_objects.js

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -52,6 +52,11 @@ console.log(typeof JsUsers[mySym])  // DOUBT - Hitesh Choudhary said it is corre
 // However Syntax for symbol to used in object is perfectly right.
 console.log(JsUsers[mySym])//jain
 
+
+JsUser.email="hitesh@chatpgt.com"
+Object.freeze(JsUser)// Now you cannot update key value pairs of object.
+JsUser.email="hitesh@microsoft.com" // This will not give error but change will not reflect when you will print JsUser.
+
 JsUsers.greeting = function(){
     console.log("Hello Js User")
 }
